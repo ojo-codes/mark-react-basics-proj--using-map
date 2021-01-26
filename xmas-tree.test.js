66fad59cf8e2a2b98e0c1f7ebf6782c743ab106b
richardcrng@gmail.com <richardcrng@gmail.com>
2021-01-26 20:01:48 +0000

    Add tests for makeFoliageLevel

--- xmas-tree.test.js
+++ xmas-tree.test.js
@@ -18,6 +18,13 @@ test("findLineWidth finds the line width from a foliage height", () => {
   expect(findLineWidth(3)).toBe(5);
 });
 
+test("makeFoliageLevel returns the foliage for a given level of a tree of given foliage height", () => {
+  expect(makeFoliageLevel(5, 1)).toBe("____#____");
+  expect(makeFoliageLevel(5, 4)).toBe("_#######_");
+  expect(makeFoliageLevel(3, 1)).toBe("__#__");
+  expect(makeFoliageLevel(3, 2)).toBe("_###_");
+});
+
 test("makeTreeTrunk returns the tree trunk part of the Christmas tree with given foliage height", () => {
   expect(makeTreeTrunk(5)).toEqual(["____#____", "____#____"]);
   expect(makeTreeTrunk(3)).toEqual(["__#__", "__#__"]);
