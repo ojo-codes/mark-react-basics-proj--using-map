1f450af5d6454519c57a6954a2206dbb9a1a64ca
Richard Ng <richardcrng@gmail.com>
2021-01-26 15:42:45 +0000

    Write initial tests for xmasTree, findLineWidth and makeTreeTrunk

--- xmas-tree.test.js
+++ xmas-tree.test.js
@@ -1,6 +1,30 @@
-import { xmasTree } from "./xmas-tree";
+import { xmasTree, findLineWidth, makeTreeTrunk } from "./xmas-tree";
 
-test("xmasTree", () => {
+test("xmasTree returns an array modelling a Christmas tree of given height", () => {
   expect(xmasTree(5)).toEqual(["____#____", "____#____"]);
   expect(xmasTree(3)).toEqual(["__#__", "__#__"]);
+});
+
+test("findLineWidth finds the line width from a height", () => {
+  expect(findLineWidth(5)).toBe(9);
+  expect(findLineWidth(3)).toBe(5);
+});
+
+test("makeTreeTrunk returns the tree trunk part of the Christmas tree with given height", () => {
+  expect(makeTreeTrunk(5)).toEqual([
+    "____#____",
+    "___###___",
+    "__#####__",
+    "_#######_",
+    "#########",
+    "____#____",
+    "____#____",
+  ]);
+  expect(makeTreeTrunk(3)).toEqual([
+    "__#__",
+    "_###_",
+    "#####",
+    "__#__",
+    "__#__",
+  ]);
 });
