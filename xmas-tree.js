76c98f1f1aeb439aff62e9fd86f1e5eb1aa425f0
Richard Ng <richardcrng@gmail.com>
2021-01-29 12:20:44 +0000

    Add comment on preferring a map

--- xmas-tree.js
+++ xmas-tree.js
@@ -14,6 +14,7 @@ function makeFoliageSegment(foliageHeight, segmentLevel) {
 }
 
 function makeTreeFoliage(foliageHeight) {
+  // An array map would be preferred here, but a for loop has been chosen for exposure
   const foliage = [];
   for (let segmentLevel = 1; segmentLevel <= foliageHeight; segmentLevel++) {
     const foliageSegment = makeFoliageSegment(foliageHeight, segmentLevel);
