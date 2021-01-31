141a5da932e959cdc9a834e6748cf3b77f2d68fc
Richard Ng <richardcrng@gmail.com>
2021-01-31 12:35:18 +0000

    Write code for isFizz, isBuzz and isFizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,44 +1,24 @@
-function xmasTree(foliageHeight) {
-  // Array spread would be preferred for brevity, but an array concatenation has been chosen for accessibility
-  const foliage = makeTreeFoliage(foliageHeight);
-  const trunk = makeTreeTrunk(foliageHeight);
-  return foliage.concat(trunk);
+function fizzbuzz(sequenceStop) {}
+
+function isFizz(n) {
+  return n % 3 === 0;
 }
 
-function findLineWidth(foliageHeight) {
-  return 2 * foliageHeight - 1;
+function isBuzz(n) {
+  return n % 5 === 0;
 }
 
-function makeFoliageSegment(foliageHeight, segmentLevel) {
-  const lineWidth = findLineWidth(foliageHeight);
-  const foliage = "#".repeat(2 * segmentLevel - 1);
-  const halfTrunkPadding = "_".repeat((lineWidth - foliage.length) / 2);
-  return `${halfTrunkPadding}${foliage}${halfTrunkPadding}`;
+function isFizzbuzz(n) {
+  return isFizz(n) && isBuzz(n);
 }
 
-function makeTreeFoliage(foliageHeight) {
-  // An array map would be preferred here, but a for loop has been chosen for exposure
-  const foliage = [];
-  for (let segmentLevel = 1; segmentLevel <= foliageHeight; segmentLevel++) {
-    const foliageSegment = makeFoliageSegment(foliageHeight, segmentLevel);
-    foliage.push(foliageSegment);
-  }
-  return foliage;
-}
+function toFizzbuzz(n) {}
 
-function makeTreeTrunk(foliageHeight) {
-  const lineWidth = findLineWidth(foliageHeight);
-  const halfTrunkWidth = (lineWidth - 1) / 2;
-  const halfTrunkPadding = "_".repeat(halfTrunkWidth);
-  const trunkSegment = `${halfTrunkPadding}#${halfTrunkPadding}`;
-  return [trunkSegment, trunkSegment];
-}
-
-// exports the functions so that ./xmas-tree.test.js can import them
+// Export the functions so that they can be used in ./fizzbuzz.test.js
 module.exports = {
-  xmasTree,
-  findLineWidth,
-  makeFoliageSegment,
-  makeTreeFoliage,
-  makeTreeTrunk,
+  fizzbuzz,
+  isFizz,
+  isBuzz,
+  isFizzbuzz,
+  toFizzbuzz,
 };
