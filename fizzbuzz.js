56e0700ae1dfa18d6d1689635f38b6921cc54e0e
Richard Ng <richardcrng@gmail.com>
2021-01-31 16:19:34 +0000

    Demo variable declaration and assignment on different lines

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,11 @@ function fizzbuzz(endNumber) {
   //  (although you could investigate JS array maps yourself)
   let sequence = [];
   for (let n = 1; n <= endNumber; n++) {
-    let nextFizzbuzzEntry = toFizzbuzz(n);
+    // demoing variable declaration and assignment on separate lines below
+    // although it'd be more typical in this case to do a one liner
+    let nextFizzbuzzEntry;
+    nextFizzbuzzEntry = toFizzbuzz(n);
+    // let nextFizzbuzzEntry = toFizzbuzz(n) // <-- what we would normally do
     sequence.push(nextFizzbuzzEntry);
   }
   return sequence;
@@ -37,6 +41,9 @@ function toFizzbuzz(n) {
 // let isBuzz = (n) => {
 //   return n % 5 === 0;
 // }
+
+/* Ignore the commented-out code below until Exercise 4 */
+// let isBuzz = (n) => n % 5 === 0;
 
 // Export the functions so that they can be used in ./fizzbuzz.test.js
 module.exports = {
