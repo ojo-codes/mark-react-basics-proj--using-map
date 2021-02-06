8f5c3dc966a1e30e8b5effc87d0a411235e675e9
Richard Ng <richardcrng@gmail.com>
2021-02-06 10:51:56 +0000

    Add example for pick initials

--- src/1-examples/1-pick-initials.test.ts
+++ src/1-examples/1-pick-initials.test.ts
@@ -1,4 +1,4 @@
-import { doubleNumber, doubleNumbers } from "./0-double-numbers";
+import { pickInitials } from "./1-pick-initials";
 
 describe("pickInitials - takes an array of objects and returns an array of strings representing initials", () => {
   test("array of objects that all have firstName and lastName", () => {
@@ -10,7 +10,10 @@ describe("pickInitials - takes an array of objects and returns an array of strin
   });
 
   test("only takes the first initial from last name (ignores double-barrel cases)", () => {
-    const input = [{ firstName: "Esme", lastName: "Hotston Moore" }];
-    expect(pickInitials(input)).toStrictEqual(["E.H."]);
+    const input = [
+      { firstName: "Esme", lastName: "Hotston Moore" },
+      { firstName: "Neil", lastName: "Patrick Harris" },
+    ];
+    expect(pickInitials(input)).toStrictEqual(["E.H.", "N.P."]);
   });
 });
