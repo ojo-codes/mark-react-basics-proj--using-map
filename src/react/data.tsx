fa2d2e6770fc124641853721290841669bb48125
Emmanuel Ojo <emmanuel.ojo97@gmail.com>
2021-02-11 16:02:38 +0000

    finished

--- src/react/data.tsx
+++ src/react/data.tsx
@@ -9,15 +9,16 @@ const shoppingList: ShoppingListItem[] = [
   { ingredient: "avocados", quantity: "excessively many" },
 ];
 
-const shoppingListItemToString = (item: ShoppingListItem) =>
-  `${item.quantity} ${item.ingredient}`;
+const shoppingListItemToString = (props: ShoppingListItem) =>
+  `${props.quantity} ${props.ingredient}`;
 
 export const mappedShoppingListStrings = shoppingList.map(
   shoppingListItemToString
 );
 
+
+
 // Read: https://reactjs.org/docs/lists-and-keys.html
 // (using index as key for simplicity for now)
 export const mappedShoppingListElements = shoppingList.map((item, idx) => (
-  <li key={idx}>{shoppingListItemToString(item)}</li>
-));
+  <li key={idx}>{shoppingListItemToString(item)}</li>))
